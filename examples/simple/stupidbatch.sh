45d822f06856dd847e2c1c056caf27e48d7f2406
Seija Sirkiä <sesirkia@csc.fi>
2017-03-28 10:30:08 +0300

    Added the simple example

---
+++ examples/simple/stupidbatch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J r_job
+#SBATCH -o output.txt
+#SBATCH -e errors.txt
+#SBATCH -t 0:01:00
+#SBATCH -p test
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=1000
+#
+module load r-env
+srun Rscript --slave stupidrcode.R
---
+++ examples/simple/stupidrcode.R
@@ -0,0 +1,2 @@
+x<-scan("stupidinput.txt")
+write(file="stupidoutput.txt",median(x))
